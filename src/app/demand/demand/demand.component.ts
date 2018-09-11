09482bd3db9503878d8f260ee9437c188dd53cca
钟兆立 <525966315@qq.com>
2018-09-11 20:51:32 +0800

    env

--- src/app/demand/demand/demand.component.ts
+++ src/app/demand/demand/demand.component.ts
@@ -13,36 +13,56 @@ export class DemandComponent implements OnInit {
   page: any;
   new_sj: any;
   show_sj: any;
+  can_get: boolean;
+  no_sj: boolean;
   ngOnInit() {
     this.page = 1;
     this.show_sj = [];
-    this.get_sj(this.page);
+    this.no_sj = false;
     let that = this;
+
+    scs_loading();
+    this.get_sj(this.page);
+
     $(window).scroll(function (){
+      this.can_get = true;
       that.loadmore($(this));
     });
   }
   loadmore(obj){
     var scrollHeight = $("html").get(0).scrollHeight;
-    var scrollTop =  $("html").get(0).scrollTop;
+    var scrollTop =  Math.ceil($("html").get(0).scrollTop);
     var windowHeight = $(obj).height();
 
 
-    if (scrollTop + windowHeight >= scrollHeight) {
+    if (scrollTop + windowHeight >= scrollHeight && this.can_get) {
       this.page = this.page + 1;
       this.get_sj(this.page);
     }
   }
   get_sj(page){
-    scs_loading();
+    if(this.no_sj){
+      return;
+    }
     this.apise.demand_index(page).subscribe(t => {
-      close_dialog();
-      this.new_sj = t;
-      for(let i = 0; i < t.data.length; i++){
-        this.show_sj.push(t.data[i]);
-      }
+        close_dialog();
+        this.new_sj = t;
+        for (let i = 0; i < t.data.length; i++) {
+            this.show_sj.push(t.data[i]);
+        }
+        this.can_get = true;
+        if(t.last_page != t.current_page){
+            setTimeout(() => {
+                if ($("body").get(0).offsetHeight <= $(window).height()) {
+                    this.page = this.page + 1;
+                    this.get_sj(this.page);
+                }
+            }, 0);
+        }
+        else{
+          this.no_sj = true;
+        }
     },error => {
-      close_dialog();
       scs_alert(error.error.message);
     })
   }
--- src/app/entity/Demand.ts
+++ src/app/entity/Demand.ts
@@ -2,4 +2,5 @@ export interface Demand{
     data:any,
     last_page: string,
     from: string,
+    current_page: string
 }

4a6c1d0c434613e84737a6d531f3417e7b9f34e6
钟兆立 <525966315@qq.com>
2018-12-17 19:55:07 +0800

    游园

--- src/app/admin-event-garden/admin-event-garden/admin-event-garden.component.ts
+++ src/app/admin-event-garden/admin-event-garden/admin-event-garden.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../api.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-admin-event-garden',
@@ -11,14 +12,19 @@ export class AdminEventGardenComponent implements OnInit {
   constructor(private apise: ApiService) { }
 
   status: string;
+  jump_url: any;
 
   ngOnInit() {
+    this.jump_url = environment.url.jump_login;
     let user_id = location.hash.split("/user/")[1];
     let event_id = location.hash.split("/user/")[0].split("/admin-event-garden/")[1];
 
     this.apise.admin_event_garden(event_id, user_id).subscribe(t => {
       this.status = "ok";
     },error => {
+      if(error.status === 401){
+        window.location.href = this.jump_url;
+      }
       this.status = error.error.message;
     });
   }
